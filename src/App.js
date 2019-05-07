6f09e069862157289239510a5f087ce03264a5d4
luwenchun <luwenchun@users.noreply.github.com>
2019-05-07 15:05:29 +0800

    react componentDidMount用来，render之后执行 用来请求后台接口数据，setstate数据，尽量减少setstate使用 一个react生命周期当中多次setstate只会触发一次视图渲染， componentWillUnmount用来清除一次性组件，如定时器或者加载一次动画等 常用来组件性能优化
    setstate传入对象进行更新操作
    setstate是异步操作可以通过传入函数形式解决函数执行的是传入更新对象
    this.setstate((prevstate,prevprops)=>({
          count:prevstate.count+1
    }))

--- src/App.js
+++ src/App.js
@@ -6,22 +6,64 @@ function Welcome(props){
  return <div>hello</div>
 }
 // 第一种组件写法
-function App() {
-  return (
-    <div className="App">
-          Learn React
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//           Learn React
+//     </div>
+//   );
+// }
+// export default App;
+// 函数型组件传递props
+function Welcome1(props) {
+  return <div>Hello, {props.name}</div>;
 }
-export default App;
 
 // 第二种组件写法
-export default class App extends Component{
+export default class App extends React.Component{
   constructor(props){
     super(props)
     this.state={
       count:0,
-      data:new Date()
+      date:new Date()
     }
+  }
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      // 3.更新状态
+      this.setState({
+        date: new Date(),
+        count: this.state.count + 1
+      });
+    }, 1000);
+    // 注意2：setState()异步的
+    this.setState((prevState, prevProps)=>({
+        count: prevState.count + 1
+    }), () => {
+        console.log(this.state.count);
+    });
+    // console.log(this.state.count);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+    formatName(user) {
+    return user.firstName + " " + user.lastName;
+  }
+  render(){
+    const name="jerry"
+    const jsx=<p>hello world</p>
+      return(
+        <div>
+           <h1>{name}</h1>
+        <p>{this.formatName({ firstName: "tom", lastName: "jerry" })}</p>
+            {/* jsx也是表达式 */}
+           {jsx}
+          {/* 组件属性传值: 传入属性是只读的 */}
+        <Welcome1 name="tom" />
+          {/* 使用状态 */}
+        <p>{this.state.date.toLocaleTimeString()}</p>
+        </div>
+      )
   }
 }
