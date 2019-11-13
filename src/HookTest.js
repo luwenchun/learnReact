e8facb3a65d50790f369d2983e00fec1bf6a8449
luwenchun <luwenchun@users.noreply.github.com>
2019-11-13 15:21:21 +0800

    // 使用useEffect操作副作用   //   请务必设置依赖选项，如果没有则设置空数组表示仅执行一次

--- src/HookTest.js
+++ src/HookTest.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
+import { setTimeout } from 'timers';
 
 function FruitList({fruits,setFruit}){
     return (
@@ -30,8 +31,22 @@ function FruitAdd(props) {
   
 export default function HookTest() {
     const [fruit,setFruit]=useState('草莓')
-    const [fruits,setFruits]=useState(['草莓','香蕉'])
-
+    const [fruits,setFruits]=useState([])
+    const [loading,setLoad]=useState(true)
+    // 使用useEffect操作副作用
+  //   请务必设置依赖选项，如果没有则设置空数组表示仅执行一次
+    useEffect(() => {
+        console.log('render')
+        setTimeout(()=>{
+            setFruits(['草莓','香蕉'])
+            setLoad(false)
+        },2000)
+    },[])
+    if(loading){
+        return (
+            <h1>loading 淡定</h1>
+        )
+    }
     return (
         <div>
             <p>{fruit===""?'请选择水果':`你选择的水果是${fruit}`}</p>
