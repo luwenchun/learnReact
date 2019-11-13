cf360c806c1ac2b029fc62a1c6451cae878fc914
luwenchun <luwenchun@users.noreply.github.com>
2019-11-13 19:03:42 +0800

    修改状态移动到全局 useReducer   // 参数一是相关reducer，参数二是初始值   // const [fruits, dispatch] = useReducer(fruitReducer, []);

--- src/HookTest.js
+++ src/HookTest.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState,useEffect,useReducer } from 'react';
 import { setTimeout } from 'timers';
 
 function FruitList({fruits,setFruit}){
@@ -28,17 +28,30 @@ function FruitAdd(props) {
         </div>
       )
 }
-  
+  //修改状态移动到全局 useReducer
+
+  function fruitReducer(state,action){
+      switch(action.type){
+        case 'init':
+            return action.payload;
+        case 'add':
+            return [...state,action.payload];
+        default:
+            return state;
+      }
+  }
 export default function HookTest() {
     const [fruit,setFruit]=useState('草莓')
-    const [fruits,setFruits]=useState([])
+    // const [fruits,setFruits]=useState([])
+    const [fruits, dispatch] = useReducer(fruitReducer, []);
     const [loading,setLoad]=useState(true)
     // 使用useEffect操作副作用
   //   请务必设置依赖选项，如果没有则设置空数组表示仅执行一次
     useEffect(() => {
         console.log('render')
         setTimeout(()=>{
-            setFruits(['草莓','香蕉'])
+            // setFruits(['草莓','香蕉'])
+            dispatch({type:'init',payload:['草莓','香蕉']})
             setLoad(false)
         },2000)
     },[])
@@ -47,16 +60,16 @@ export default function HookTest() {
         document.title=fruit
     },[fruit])
     // 副作用清除
-    useEffect(()=>{
-      var timer=  setInterval(()=>{
-           console.log("应用启用")
-        },1000)
+    // useEffect(()=>{
+    //   var timer=  setInterval(()=>{
+    //        console.log("应用启用")
+    //     },1000)
 
-        return () => {
-            console.log("应用卸载")
-            clearInterval(timer)
-        };
-    },[])
+    //     return () => {
+    //         console.log("应用卸载")
+    //         clearInterval(timer)
+    //     };
+    // },[])
 
     if(loading){
         return (
@@ -66,7 +79,8 @@ export default function HookTest() {
     return (
         <div>
             <p>{fruit===""?'请选择水果':`你选择的水果是${fruit}`}</p>
-            <FruitAdd  onAddFruit={pname=>setFruits([...fruits,pname])}></FruitAdd>
+            {/* <FruitAdd  onAddFruit={pname=>setFruits([...fruits,pname])}></FruitAdd> */}
+            <FruitAdd  onAddFruit={pname=>dispatch({type:'add',payload:pname})}></FruitAdd>
             <FruitList fruits={fruits} setFruit={setFruit}></FruitList>
            
         </div>
